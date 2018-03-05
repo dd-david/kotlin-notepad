14953cb58bb96b0e7af819fb1e69288c72bc9f3b
dd-david <dev.dream.developer@gmail.com>
2018-03-05 23:37:23 +0900

    Apply kotlin-android-extensions [Udacity-1-8]

--- app/src/main/java/com/udacity/notepad/MainActivity.kt
+++ app/src/main/java/com/udacity/notepad/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.udacity.notepad
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-
 import com.udacity.notepad.crud.CreateActivity
 import com.udacity.notepad.recycler.NotesAdapter
 import com.udacity.notepad.util.SpaceItemDecoration
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
+/*
+* https://kotlinlang.org/docs/tutorials/android-plugin.html
+*
+* View Binding
+*   findViewById() causes potential bugs, so several libraries, like ButterKnife,
+*   help to this issue with annotations.
+*   Kotlin extension plugin allows us to obtain same experience without adding extra codes
+*
+* Step 1. How to use Kotlin Extension Library
+*   Android extension is part of Kotlin plugin for IntelliJ IDEA and AndroidStudio.
+*   You do not need to additional install procedure.
+*   Just add in build.gradle file
+*   (ex) apply plugin: 'kotlin-android-extensions'
+*
+* Step 2. Import synthetic properties
+*   It's convenient to import all widget properties for a specific layout in one go.
+*   (ex) import kotlinx.android.synthetic.main.<layout>.*
+*
+* Todo: Experimental Mode (not ready yet)
+*   androidExtensions {
+*     experimental = true
+*   }
+*
+* */
 class MainActivity : AppCompatActivity() {
-
-    private var recycler: RecyclerView? = null
 
     /*
     * https://kotlinlang.org/docs/reference/keyword-reference.html#modifier-keywords
@@ -29,21 +48,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { startActivity(CreateActivity.get(this@MainActivity)) }
 
-        recycler = findViewById(R.id.recycler)
         /*
         * https://kotlinlang.org/docs/reference/null-safety.html#the--operator
         * !! Operation : not-null assertion operation
         *                if null, throws an exception
+        *
+        * After applying kotlin extension plugin, !! is removed,
+        * because it automatically knows nullable or not.
         * */
-        recycler!!.layoutManager = LinearLayoutManager(this)
-        recycler!!.addItemDecoration(SpaceItemDecoration(this, R.dimen.margin_small))
-        recycler!!.adapter = NotesAdapter(this)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.addItemDecoration(SpaceItemDecoration(this, R.dimen.margin_small))
+        recycler.adapter = NotesAdapter(this)
     }
 
     override fun onResume() {
@@ -53,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     public override fun onDestroy() {
         super.onDestroy()
-        recycler!!.adapter = null
+        recycler.adapter = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -83,6 +101,6 @@ class MainActivity : AppCompatActivity() {
         *       which returns null on failure
         *       (ex) val x : String? = y as? String   // right side of <as?> may return nullable on failure
         * */
-        (recycler!!.adapter as NotesAdapter).refresh()
+        (recycler.adapter as NotesAdapter).refresh()
     }
 }
